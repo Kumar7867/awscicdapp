74d1530430c5bacb7589666383f047978df8cb94
kumar7867 <tirumalaikumargn@gmail.com>
2026-02-19 17:26:03 +0000

    python command update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-apt update -y
-apt install python3-pip -y
-pip3 install -r /home/ubuntu/app/requirements.txt
+set -e
+
+sudo apt update -y
+sudo apt install python3 python3-venv python3-pip -y
+
+cd /home/ubuntu/awscicdapp
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Upgrade pip inside venv
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
